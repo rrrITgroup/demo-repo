0fd211ab571980f8e32f4f8b067ee6ee3ea2b23e
sdjkg@skjdfhs.com <sdjkg@skjdfhs.com>
2022-05-24 07:32:48 +0530

    init

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+/**
+ * @param {typeof sap.ui.ore.mvc.Controller} Controller
+ */
+function (Controller) {
+	"use strict";
+
+	return Controller.extend("ns.ui5-demo.controller.View1", {
+		onInit: function () {
+
+		},
+		
+	onShowHello: function () {
+		// show a native JavaScript alert
+		//updated on  April 26
+
+		alert("Hello Team!");
+		}
+	});
+});
